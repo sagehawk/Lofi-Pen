cd39f8f2c8f61375d4cb82390f0edd7252550b6b
Sajjad <haq.sajjad220@gmail.com>
2022-03-29 03:47:55 -0500

    Publishing

---
+++ js/default.js
@@ -0,0 +1,15 @@
+var defaultTitle = 'This is LofiPen';
+var defaultContent =
+'<p>\
+A chill writing zone where you can write freely and unapologetically.  \
+</p>\
+<p> \
+	To get started, all you need to do is delete this text (seriously, just highlight it and hit delete), and fill the page with your own fantastic words. You can even change the title!  \
+</p> \
+<p> \
+	You can use <b>bold</b>, <i>italics</i>, <b><i>both</i></b> and <a href="http://Lofipen.io"> urls </a> just by highlighting the text and selecting them from the tiny options box that appears above it.\
+</p>\
+<blockquote>\
+	Quotes are easy to add too!\
+</blockquote>\
+<p>Happy Typing! ~ <b>Sajjad Haq</b></p>';
---
+++ js/utils.js
@@ -0,0 +1,41 @@
+// Utility functions
+LofiPen = window.LofiPen || {};
+LofiPen.util = (function() {
+
+	function supportsHtmlStorage() {
+		try {
+		    return 'localStorage' in window && window['localStorage'] !== null;
+		} catch (e) {
+		    return false;
+		}
+	};
+
+	function getText(el) {
+		var ret = " ";
+		var length = el.childNodes.length;
+		for(var i = 0; i < length; i++) {
+		    var node = el.childNodes[i];
+		    if(node.nodeType != 8) {
+
+			if ( node.nodeType != 1 ) {
+			    // Strip white space.
+			    ret += node.nodeValue;
+			} else {
+			    ret += getText( node );
+			}
+		    }
+		}
+		return LofiPen.util.trim(ret);
+	};
+
+	function trim(string) { 
+		return string.replace(/^\s+|\s+$/g, ''); 
+	};
+
+	return {
+		trim: trim,
+		getText: getText,
+		supportsHtmlStorage: supportsHtmlStorage
+	}
+
+})()
