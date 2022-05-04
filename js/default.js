9a1b2169ac136a8d271634e293457c920d65257e
Sajjad <haq.sajjad220@gmail.com>
2022-05-04 08:17:38 -0500

    Clean up

--- js/default.js
+++ js/default.js
@@ -1,13 +1,13 @@
 var defaultTitle = 'This is LofiPen';
 var defaultContent =
 '<p>\
-A knock off website of zenpen.io with lofi radio playing in the background. Have a chill writing experience and write freely because everything is stored locally.  \
+Enjoy a chill writing experience and write freely because everything is stored locally.  \
 </p>\
 <p> \
 	To get started, all you need to do is delete this text (seriously, just highlight it and hit delete), and fill the page with your own fantastic words. You can even change the title!  \
 </p> \
 <p> \
-	You can use <b>bold</b>, <i>italics</i>, <b><i>both</i></b> and <a href="http://Lofipen.io"> urls </a> just by highlighting the text and selecting them from the tiny options box that appears above it.\
+	You can use <b>bold</b>, <i>italics</i>, <b><i>both</i></b> and <a href="http://Lofipen.com"> urls </a> just by highlighting the text and selecting them from the tiny options box that appears above it.\
 </p>\
 <blockquote>\
 	Quotes are easy to add too!\
